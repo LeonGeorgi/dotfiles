f8b77dd8d9225b66a8136385ba654ba2940e54c7
Leon Georgi <leon@ifsr.de>
2018-12-27 19:23:46 +0100

    Update polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar top &
-polybar bottom &
+polybar top -c $HOME/.config/polybar/config &
+polybar bottom -c $HOME/.config/polybar/config &
 
 echo "Bars launched..."
---
+++ polybar/toggle.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Toggle bar visibility
+xdo show -N "Polybar"
+sleep 1
+xdo hide -N "Polybar"
